f1f32e4ca89dd4c2852c89c70bd1d942d20a670d
Alan Bacon <alan.bacon@bbc.co.uk>
2018-10-10 19:31:06 +0100

    refactor reducers into separate functions for each bit of state

--- www/scripts/storeCtrl/reducers.js
+++ www/scripts/storeCtrl/reducers.js
@@ -3,125 +3,137 @@ import actionNames from "./actionNames";
 const MAX_ITEM_QUANTITY = 9;
 const MIN_ITEM_QUANTITY = 1;
 
-const initialState = {
-  items: [],
-  summaryCount: 0,
-  selectedItem: {},
-  isLoading: false
-};
+// const initialState = {
+//   items: [],
+//   summaryCount: 0,
+//   selectedItem: {},
+//   isLoading: false
+// };
 
-const getItemFromId = (id, items) => {
+function getItemFromId (id, items) {
   for(let i = 0; i < items.length; i++) {
     if (items[i].id === id)
       return items[i];
   }
 };
 
-const getItemIndexFromId = (id, items) => {
+function getItemIndexFromId (id, items) {
   for(let i = 0; i < items.length; i++)
     if (items[i].id === id)
       return i;
 };
 
-const rootReducer = (state=initialState, action) => {
+function items (items=[], action) {
   switch (action.type) {
 
-    case actionNames.ADD_ITEM: {
-      let items = [...state.items, action.payload];
-      return ({...state, ...{items}});
-    }
-
     case actionNames.ADD_ITEMS: {
-      let items = [...state.items, ...action.payload];
-      return ({...state, ...{items}});
+      return items = [...items, ...action.payload];
     }
 
     case actionNames.DELETE_ITEM: {
-      let itemIndex = getItemIndexFromId(action.payload, state.items);
+      let itemIndex = getItemIndexFromId(action.payload, items);
       if (itemIndex !== undefined) {
-        let items = [
-          ...state.items.slice(0,itemIndex), 
-          ...state.items.slice(itemIndex+1, state.items.length)
+        return [
+          ...items.slice(0,itemIndex), 
+          ...items.slice(itemIndex+1, items.length)
         ]
-        return ({...state, ...{items}});
       } else {
         console.warn("tried to delete node with invalid id");
-        return {...state}
+        return items
       }
     }
 
     case actionNames.SET_ITEM_NAME: {
-      let itemIndex = getItemIndexFromId(action.payload, state.items);
+      let itemIndex = getItemIndexFromId(action.payload, items);
       if (itemIndex != undefined) {
-        let newItem = state.items[itemIndex].copy()
+        let newItem = items[itemIndex].copy()           // don't mutate anything, create new objects
         newItem.name = action.payload;
-        let items = [
-          ...state.items.slice(0,itemIndex), 
+        return [
+          ...items.slice(0,itemIndex), 
           newItem,
-          ...state.items.slice(itemIndex+1, state.items.length)
+          ...items.slice(itemIndex+1, items.length)
         ]
-        return ({...state, ...{items}});
       } else {
         console.warn("tried to set item name of item with invalid id");
-        return {...state}
+        return items
       }
     }
 
     case actionNames.INC_ITEM: {
-      let itemIndex = getItemIndexFromId(action.payload.id, state.items);
+      let itemIndex = getItemIndexFromId(action.payload.id, items);
       if (itemIndex != undefined) {
-        let newItem = state.items[itemIndex].copy()
+        let newItem = items[itemIndex].copy()           // don't mutate anything, create new objects
         newItem.value += action.payload.val;
         newItem.value = Math.min(MAX_ITEM_QUANTITY, Math.max(MIN_ITEM_QUANTITY, newItem.value));
-        let items = [
-          ...state.items.slice(0,itemIndex), 
+        return [
+          ...items.slice(0,itemIndex), 
           newItem,
-          ...state.items.slice(itemIndex+1, state.items.length)
+          ...items.slice(itemIndex+1, items.length)
         ]
-        return ({...state, ...{items}});
       } else {
         console.warn("tried to increment item with invalid id");
-        return {...state}
+        return items
       }
     }
-
-    case actionNames.SELECT_ITEM: {
-      let item = getItemFromId(action.payload.id, state.items);
-      if (item != undefined) {
-        return ({
-          ...state, 
-          ...{selectedItem: item}
-        })
-      } else {
-        console.warn("tried to select item with invalid id");
-        return ({
-          ...state, 
-          ...{selectedItem: {} }
-        })
-      }
-    }
-
-    case actionNames.INC_SUMMARY: {
-      return ({
-        ...state, 
-        ...{summaryCount: (state.summaryCount + 1)}
-      }); 
-    }
-
-    case actionNames.LOADING: {
-      return ({
-        ...state, 
-        ...{isLoading: action.payload}
-      });
-    }
-
-    case actionNames.NOOP:
-      return state;
 
     default:
-      return state;
+      return items
   }
-};
+}
+
+function itemSelection (selectedItem={}, action, items) {
+  switch (action.type) {
+
+    case actionNames.SELECT_ITEM: {
+      let item = getItemFromId(action.payload.id, items);
+      if (item != undefined) {
+        return item
+      } else {
+        console.warn("tried to select item with invalid id");
+        return {}
+      }
+    }
+
+    default: 
+      return selectedItem
+
+  }
+}
+
+function summaryCount (summaryCount=0, action) {
+  switch (action.type) {
+
+    case actionNames.INC_SUMMARY: {
+      return summaryCount + 1;  
+    }
+
+    default:
+      return summaryCount
+
+  }
+}
+
+function isLoading (isLoading=0, action) {
+  switch (action.type) {
+
+    case actionNames.LOADING: {
+      return action.payload;  
+    }
+
+    default:
+      return isLoading
+
+  }
+}
+
+function rootReducer(state = {}, action) {
+  return {
+    items: items(state.items, action),
+    selectedItem: itemSelection(state.selectedItem, action, state.items),
+    summaryCount: summaryCount(state.summaryCount, action),
+    isLoading: isLoading(state.isLoading, action)
+  }
+}
 
 
 export default rootReducer;
